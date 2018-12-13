b40169016b161c239d604c07fdaeb752ce996972
Bernardo Tolosa <bernardotolosajr@gmail.com>
2018-12-13 10:26:48 +0800

    Initial Commit

---
+++ basicmvvm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  basicmvvm
+//
+//  Created by Bernard tolosa on 13/12/2018.
+//  Copyright © 2018 Mynt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
